334570195cc0416cf2d782af7c981c3cf939dca2
thiagopequeno <thiago.castro@sptech.school>
2022-12-01 13:18:09 -0300

    finalizado

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -66,7 +66,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var personagem=req.body.personagemServer
+    var personagem=req.body.fkpersonagemServer
 
     // Faça as validações dos valores
     if (nome == undefined) {
